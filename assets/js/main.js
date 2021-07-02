7a093d0332a586a8c517bee215dba07590be29bb
Lucas Samuel <lucasssdo@gmail.com>
2021-07-02 11:39:26 -0300

    testimonials--mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
-// Evento de click no nav menu
 
+// Evento de click no nav menu
 const nav = document.querySelector('#header nav');
 const toggle = document.querySelectorAll('#header .toggle');
 
@@ -8,3 +8,17 @@ for (const element of toggle) {
     nav.classList.toggle('show');
   })
 }
+
+// Swiper - Testimonials
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  spaceBetween: 24,
+  mousewheel: true,
+  keyboard: true,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+});
+
+
+
