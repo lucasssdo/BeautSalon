00f85bc346903485c9f3057dd6117e9424c68467
Lucas Samuel <lucasssdo@gmail.com>
2021-07-06 16:37:18 -0300

    header--update

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,31 @@ for (const element of itemMenu){
   })
 }
 
+/* Menu ativo conforme a seção visível na página */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
 // Ativar shadow com scroll da pagina
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
@@ -43,6 +68,7 @@ function backToTop() {
 window.addEventListener('scroll', ()=>{
   changeHeaderWhenScroll()
   backToTop()
+  activateMenuAtCurrentSection()
 })
 
 // Scroll Reveal
